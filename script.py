df324a6ddc273581155073f492a2eb04973d2d09
aljamaha <aljamaha@stanford.edu>
2019-10-30 14:05:42 -0700

    clutch play added

--- script.py
+++ script.py
@@ -1,15 +1,15 @@
 import pandas as pd
-#import matplotlib
-#matplotlib.use('TkAgg')
 from matplotlib import pyplot as plt
 import numpy as np
 from sklearn.preprocessing import MinMaxScaler
+from copy import deepcopy
+from sklearn.preprocessing import StandardScaler
 
 'loading data'
 data = pd.read_csv('shot_logs.csv')
 
-'removing unnecessary columns: GAME ID, Matchup, Winner/loser'
-data = data.drop(['GAME_ID','MATCHUP','W'], axis = 1)
+'removing unnecessary columns: GAME ID, Matchup'
+data = data.drop(['GAME_ID','MATCHUP'], axis = 1)
 
 'data size'
 #print 'Data Size: ', data.shape
@@ -41,4 +41,40 @@ plt.hist(data['CLOSE_DEF_DIST'], bins='auto')
 plt.xlim([0, 20])
 #plt.show()
 
-plt.savefig('test.pdf')
+#plt.savefig('test.pdf')
+
+'Clutch players'
+#print(data['SHOT_CLOCK'])
+print(data.shape)
+clutch = data[data['SHOT_CLOCK'] > 20.0]
+print(clutch['player_name'].value_counts()[0:20])
+#print(data_slice['SHOT_CLOCK'])
+
+
+'Normalizing Data'
+'Location'
+#there are problems here
+#data[data['LOCATION'] == 'A'] = 1
+#data[data['LOCATION'] == 'H'] = 0
+#azdias[azdias['OST_WEST_KZ'] == 'W'] = 1
+
+
+
+'''
+'Margin'#scaler = StandardScaler()
+#data_scaled = scaler.fit_transform(data)
+numerical = ['DRIBBLES']
+scaler = MinMaxScaler()
+scaler.fit(data)
+scaler.transform(data)
+#data[numerical] = scaler.fit_transform(data[numerical],feature_range=(0, 1))
+print(data['DRIBBLES'][0:20])
+#a = deepcopy(data['PERIOD'])
+#print(a.shape)
+#data['PERIOD'].reshape(-1,1)
+#a = np.reshape(data['PERIOD'], (-1, 1))
+#print(a.shape)
+#print(scaler.fit_transform(a))
+#data['PERIOD'] = scaler.fit_transform(data['PERIOD'])
+#print(data['PERIOD'])
+'''
